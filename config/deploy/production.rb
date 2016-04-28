73f6cd9f507ee65b2f06c491b482d3cdaa275a2e
William Lian <william.bs.lian@gmail.com>
2016-04-28 02:29:20 -0700

    aa

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'williamlian.com', user: 'william', roles: %w{web app db}, ssh_options: {keys: %w(/Users/william/.ssh/william_aws.pem)}
+server 'williamlian.com', user: 'william', roles: %w{app}, ssh_options: {keys: %w(/Users/william/.ssh/william_aws.pem)}
 
 
 # role-based syntax
