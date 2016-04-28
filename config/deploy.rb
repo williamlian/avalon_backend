65df14c4f0e3ff3f29ad968a8ad40e6a740f0718
William Lian <wlian@uber.com>
2016-04-28 13:36:06 -0700

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, :master
 set :deploy_to, '/home/william/avalon_backend'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.3.0' # Edit this if you are using MRI Ruby
