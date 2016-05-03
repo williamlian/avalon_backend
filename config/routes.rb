d0cc18b5fd4b000f82f39b48cb2f33213d96aa9f
William Lian <william.bs.lian@gmail.com>
2016-05-02 23:34:11 -0700

    api path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api do
+  scope '/api' do
     # post {size: int}
     # response: {group: Group, player: Player}
     post '/group' => 'group#create'
