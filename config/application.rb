7b2ad5b656882d956e44c40b69f6854a5f1743c9
William Lian <william.bs.lian@gmail.com>
2017-02-12 21:54:50 -0800

    license

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,19 @@
+# Avalon - A Board Game. Backend.
+# Copyright (C) 2017 Boshu Lian
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -30,6 +46,6 @@ module AvalonBackend
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-    
+
   end
 end
